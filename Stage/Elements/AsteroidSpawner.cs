563ee62fec36e6b3e811d93f444b86803bfdc9a4
kalin.eh <kalin.eh@44a6ae3e-fdf9-11de-b136-7f7962f7bc17>
2010-01-15 12:50:11 +0000

    Added stage, stage-definition system. Added Stage1 definition.

---
+++ Stage/Elements/AsteroidSpawner.cs
@@ -0,0 +1,36 @@
+﻿//
+// AsteroidSpawner.cs
+//
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace galaxy
+{
+    public class CStageAsteroidSpawner
+        : CStageElement
+    {
+        public float Frequency { get; set; }
+        public float IncreaseRate { get; set; }
+        public int SpawnCount { get; set; }
+
+        public override void Update(CWorld world)
+        {
+            Frequency += IncreaseRate;
+
+            if (SpawnCount > 0 && world.Random.NextFloat() < Frequency)
+            {
+                CAsteroid.Spawn(world);
+                SpawnCount -= 1;
+            }
+        }
+
+        public override bool IsExpired()
+        {
+            return SpawnCount <= 0;
+        }
+    }
+}
---
+++ Stage/Elements/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿//
+// EnemySpawner.cs
+//
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace galaxy
+{
+    public class CStageEnemySpawner
+        : CStageElement
+    {
+        public delegate CEnemy Spawner(Vector2 position);
+
+        public override void Update(CWorld world)
+        {
+        }
+
+        public override bool IsExpired()
+        {
+         	throw new NotImplementedException();
+        }
+    };
+}
+
+
