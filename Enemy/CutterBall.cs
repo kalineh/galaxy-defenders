5bc0c7ab5b35b2289fad3bca3010723fdf333275
kalin.eh <kalin.eh@44a6ae3e-fdf9-11de-b136-7f7962f7bc17>
2010-09-03 07:18:52 +0000

    Added Cutter enemy.

---
+++ Enemy/CutterBall.cs
@@ -0,0 +1,41 @@
+﻿//
+// CutterBall.cs
+//
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace Galaxy
+{
+    public class CCutterBall
+        : CEnemy
+    {
+        public CCutter Owner { get; set; }
+
+        public override void Initialize(CWorld world)
+        {
+            base.Initialize(world);
+
+            Physics = new CPhysics();
+            Collision = CCollision.GetCacheCircle(this, Vector2.Zero, 24.0f);
+            Visual = CVisual.MakeSpriteCached1(world, "Textures/Enemy/CutterBall");
+        }
+
+        public override void UpdateCollision()
+        {
+            CollisionCircle circle = Collision as CollisionCircle;
+            circle.Position = Physics.PositionPhysics.Position;
+        }
+
+        public override void TakeDamage(float damage)
+        {
+            //Owner.TakeDamage(damage);
+            CEffect.CutterBallAbsorb(World, Physics.PositionPhysics.Position, Vector2.Zero, 0.5f, new Color(252, 124, 85, 255));
+        }
+    }
+}
+
