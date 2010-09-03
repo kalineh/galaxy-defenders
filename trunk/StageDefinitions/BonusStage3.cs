c4004ea9fe6d4b700279970d7e84f13d559faf69
kalin.eh <kalin.eh@44a6ae3e-fdf9-11de-b136-7f7962f7bc17>
2010-09-03 05:47:41 +0000

    Actually added new stage files.

---
+++ trunk/StageDefinitions/BonusStage3.cs
@@ -0,0 +1,33 @@
+//
+// BonusStage3.cs
+//
+namespace Galaxy {
+namespace Stages {
+public class BonusStage3 {
+public static CStageDefinition GenerateDefinition() {
+CStageDefinition stage = new CStageDefinition("BonusStage3");
+stage.ScrollSpeed = 3.0f;
+stage.BackgroundSceneryName = "SimpleSpace";
+stage.ForegroundSceneryName = "Empty";
+stage.MusicName = "C";
+stage.AddElement(0, 
+new Galaxy.CStageElementCameraStop() {
+Position = new Microsoft.Xna.Framework.Vector2() {
+X = 0.0f,
+Y = -17584.0f,
+},
+});
+stage.AddElement(0, 
+new Galaxy.CStageElementStageFinish() {
+World = null,
+Position = new Microsoft.Xna.Framework.Vector2() {
+X = -16.0f,
+Y = -19448.0f,
+},
+});
+
+return stage;
+}
+}
+} // namespace Stages
+} // namespace Galaxy
---
+++ trunk/StageDefinitions/Stage11.cs
@@ -0,0 +1,33 @@
+//
+// Stage11.cs
+//
+namespace Galaxy {
+namespace Stages {
+public class Stage11 {
+public static CStageDefinition GenerateDefinition() {
+CStageDefinition stage = new CStageDefinition("Stage11");
+stage.ScrollSpeed = 3.0f;
+stage.BackgroundSceneryName = "SimpleSpace";
+stage.ForegroundSceneryName = "Empty";
+stage.MusicName = "A";
+stage.AddElement(0, 
+new Galaxy.CStageElementCameraStop() {
+Position = new Microsoft.Xna.Framework.Vector2() {
+X = 0.0f,
+Y = -17584.0f,
+},
+});
+stage.AddElement(0, 
+new Galaxy.CStageElementStageFinish() {
+World = null,
+Position = new Microsoft.Xna.Framework.Vector2() {
+X = -16.0f,
+Y = -19448.0f,
+},
+});
+
+return stage;
+}
+}
+} // namespace Stages
+} // namespace Galaxy
